8a1c94a9428bc083f451c4f875a0f14945ab0d55
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2020-11-28 15:30:27 -0700

    adding draft of system failure

--- atm.js
+++ atm.js
@@ -2,6 +2,18 @@ var globalInput;
 var currentUser;
 var phase = "welcome";
 var componentFailure = [];
+
+
+class CardScanner {
+    constructor(data, status, cardInserted, cardEjector, port) {
+        this.data = data;
+        this.status = status;
+        this.cardInserted = cardInserted;
+        this.cardEjector = cardEjector;
+        this.port = port
+    }
+}
+
 var user1 = {
     cardnumber: 200034568719,
     pin: 1234,
@@ -17,6 +29,7 @@ var user3 = {
     pin: 3456,
     balance: 40
 }
+
 function numberInput(input) {
     if(phase !== "welcome") {
         $('#pinField').append(input);
@@ -67,16 +80,21 @@ function ejectCard() {
 
 }
 function systemFailure() {
-
+    if (cardScanner.status === false){
+        $('#message').text("Broken System. Contact Support")
+        ejectCard()
+        alert("EXIT")
+    }
 }
 
 //References:
 // https://stackoverflow.com/questions/6312993/javascript-seconds-to-time-string-with-format-hhmmss
 // https://stackoverflow.com/questions/26584233/updating-javascript-time-every-second
-function doDate()
-{
+function doDate() {
     var time_now = "Current Time: " + new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
     $("#time-display").text(time_now);
 }
 
 setInterval(doDate, 1000);
+cardScanner = new CardScanner(undefined, true, undefined, undefined, undefined)
+systemFailure()
